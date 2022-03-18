f796c441d71be19b3a0fa0e18b89e4d8d0e5cc94
daaeev <LfdblGbhju@gmail.com>
2022-03-18 15:15:49 +0200

    Исправлена ошибки в пути к js скрипту

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -161,7 +161,7 @@
     </footer>
 </div>
 
-<script src="{{asset('s/jquery-3.3.1.min.js')}}"></script>
+<script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
 <script src="{{asset('js/jquery-ui.js')}}"></script>
 <script src="{{asset('js/popper.min.js')}}"></script>
 <script src="{{asset('js/bootstrap.min.js')}}"></script>
