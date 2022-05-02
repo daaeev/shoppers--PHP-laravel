80debcfbda02b930e7b633b541f74c79f3e2288b
daaeev <LfdblGbhju@gmail.com>
2022-05-02 11:55:38 +0300

    Убран лишний выпадающий список в верхнем меню

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -68,15 +68,6 @@
                 <ul class="site-menu js-clone-nav d-none d-md-block">
                     <li class="has-children">
                         <a href="{{route('home')}}">Home</a>
-                        <ul class="dropdown">
-                            <li><a href="#">Menu One</a></li>
-                            <li class="has-children">
-                                <a href="#">Sub Menu</a>
-                                <ul class="dropdown">
-                                    <li><a href="#">Menu One</a></li>
-                                </ul>
-                            </li>
-                        </ul>
                     </li>
                     <li>
                         <a href="{{route('about')}}">About</a>
